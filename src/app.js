30a1599bb2fb41fdb72323fc9374c76a058df844
钟文治 <13824443763@qq.com>
2020-02-26 23:29:52 +0800

    anydoor

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
 const http = require('http');
 // const chalk = require('chalk');
-// const path = require('path');
+const path = require('path');
 const conf = require('./config/defaultConfig');
-// const route = require('./helper/route');
+const route = require('./helper/route');
 // const server =
 http.createServer((req, res) => {
-  // const url = req.url;
-  // const filePath = path.join(conf.root, url);
-  // route(req, res, filePath, conf);
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('holloe  world');
-  res.end('fdsa');
+  const url = req.url;
+  const filePath = path.join(conf.root, url);
+  route(req, res, filePath, conf);
 }).listen(conf.port);
 
 // server.listen(conf.port, conf.hostname, () => {
