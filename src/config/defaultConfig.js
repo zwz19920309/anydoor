532cae9d4c11f28b3d7f2c8defa176f918b46c60
钟文治 <13824443763@qq.com>
2020-02-26 23:11:37 +0800

    anydoor

--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: process.cwd(),
   hostname: '127.0.0.1',
-  port: '9527',
+  port: '3005',
   compress: /\.(html|js|css|md)/,
   cache: {
     maxAge: 600,
---
+++ src/test.js
@@ -0,0 +1,12 @@
+var http = require('http')
+http.createServer(function (req, res) {//回调函数
+  console.info(req.httpVersion);
+  console.info(req.headers);
+  console.info(req.method);
+  console.info(req.url);
+  console.info(req.trailers);
+  console.info(req.complete);
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('holloe  world');
+  res.end('fdsa');
+}).listen(8000);
