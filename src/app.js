01e9e224a561ab5a7ec212ebc925de6968b33483
钟文治 <13824443763@qq.com>
2020-02-26 23:31:37 +0800

    anydoor

--- src/app.js
+++ src/app.js
@@ -3,36 +3,29 @@ const http = require('http');
 const path = require('path');
 const conf = require('./config/defaultConfig');
 const route = require('./helper/route');
-// const server =
-http.createServer((req, res) => {
-  const url = req.url;
-  const filePath = path.join(conf.root, url);
-  route(req, res, filePath, conf);
-}).listen(conf.port);
+class Server {
 
-// server.listen(conf.port, conf.hostname, () => {
-//   const addr = `http://${conf.hostname}:${conf.port}`;
-//   console.info(`u Server started at ${chalk.green(addr)}`);
-// });
-// class Server {
+  constructor(config) {
+    this.conf = Object.assign({}, conf, config);
+  }
 
-//   constructor(config) {
-//     this.conf = Object.assign({}, conf, config);
-//   }
+  start() {
+    http.createServer((req, res) => {
+      const url = req.url;
+      const filePath = path.join(conf.root, url);
+      route(req, res, filePath, conf);
+    }).listen(conf.port);
+    // const server = http.createServer((req, res) => {
+    //   const url = req.url;
+    //   const filePath = path.join(this.conf.root, url);
+    //   route(req, res, filePath, this.conf);
+    // });
 
-//   start() {
-//     const server = http.createServer((req, res) => {
-//       const url = req.url;
-//       const filePath = path.join(this.conf.root, url);
-//       route(req, res, filePath, this.conf);
-//     });
+    // server.listen(this.conf.port, this.conf.hostname, () => {
+    //   const addr = `http://${conf.hostname}:${this.conf.port}`;
+    //   console.info(`u Server started at ${chalk.green(addr)}`);
+    // });
+  }
+}
 
-//     server.listen(this.conf.port, this.conf.hostname, () => {
-//       const addr = `http://${conf.hostname}:${this.conf.port}`;
-//       console.info(`u Server started at ${chalk.green(addr)}`);
-//     });
-//   }
-
-// }
-
-// module.exports = Server;
+module.exports = Server;
